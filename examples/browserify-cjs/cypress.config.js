7ed2c4b47ae445b2593af4aba8ad1b459f6e0a44
amanchopra1905 <amanc@lambdatest.com>
2024-07-23 15:47:38 +0530

    updated the repo code files

--- examples/browserify-cjs/cypress.config.js
+++ examples/browserify-cjs/cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
-const {addCucumberPreprocessorPlugin} = require("@badeball/cypress-cucumber-preprocessor");
-const {preprocessor} = require("@badeball/cypress-cucumber-preprocessor/browserify");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  preprocessor,
+} = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
 async function setupNodeEvents(on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
